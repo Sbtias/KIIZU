b000b173ac3d82bfad6ab9695e7e500229de6a7a
Sbtias <sebastianmartinezrodriguez2023@gmail.com>
2026-09-18 09:18:14 -0400

    Forzar carga de navegación actualizada

--- js/creator.js
+++ js/creator.js
@@ -1,5 +1,5 @@
 import { supabase } from "./app.js";
-import { bootShell } from "./nav.js";
+import { bootShell } from "./nav.js?v=20260918-1335";
 import { toast, setBusy } from "./ui.js";
 
 const state=await bootShell();
