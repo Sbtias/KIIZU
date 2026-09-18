bce9a256f42cb7f60c631ccfd78f8acf990b16c7
Sbtias <sebastianmartinezrodriguez2023@gmail.com>
2026-09-18 15:49:09 -0400

    Refresh navigation settings module

--- js/profile.js
+++ js/profile.js
@@ -1,5 +1,5 @@
 import { supabase } from "./app.js";
-import { bootShell } from "./nav.js?v=20260918-1335";
+import { bootShell } from "./nav.js?v=20260918-2010";
 import { toast } from "./ui.js";
 
 export async function getProfile(userId) {
