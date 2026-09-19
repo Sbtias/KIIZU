65ead3a639ea32fd97c3664efe879629ca70f612
Sbtias <sebastianmartinezrodriguez2023@gmail.com>
2026-09-19 10:24:26 -0400

    Refresh navigation module cache

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
 import { supabase } from "./app.js";
-import { bootShell } from "./nav.js?v=20260918-1335";
+import { bootShell } from "./nav.js?v=20260919-1426";
 import { toast } from "./ui.js";
 import { Kiizu2D, buildWorld, buildWorldFromConfig } from "./game-engine.js";
 
