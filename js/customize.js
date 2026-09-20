9f9ff9fd6a8e0d24fb599e205181b8e89242a45d
Sbtias <sebastianmartinezrodriguez2023@gmail.com>
2026-09-19 22:29:07 -0400

    chore: refresh nav cache for premium subscription

--- js/customize.js
+++ js/customize.js
@@ -1,5 +1,5 @@
 import { supabase } from "./app.js";
-import { bootShell } from "./nav.js?v=20260919-1426";
+import { bootShell } from "./nav.js?v=20260920-0330";
 import { toast } from "./ui.js";
 
 const state = await bootShell();
